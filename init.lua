64280e7786c10e5dea99ed72f830613b82c5c24a
John Crannell-Ash <Jcrash29@jcrash.com>
2019-05-11 20:16:50 -0400

    We can now properly post a tweet onto twitter. Need to update documentation and add the extra functionality

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 require("tweet");
 
+print("HEllO world")
 
 lighton=0
 pin=4
@@ -8,11 +9,17 @@ gpio.mode(pin,gpio.OUTPUT)
 enduser_setup.start(
   function()
     print("Connected to wifi as:" .. wifi.sta.getip())
+    dofile('eus_params.lua')
+    print("Twitter Key: " .. twitter_key)
+    setAPIKey(twitter_key)
+    sendTweet("Welcome to Tweet8266 bot")
   end,
   function(err, str)
     print("enduser_setup: Err #" .. err .. ": " .. str)
   end
 );
+
+print("GOT HERE");
 
 local mytimer = tmr.create()
 mytimer:register(2000, tmr.ALARM_AUTO, function ()    
